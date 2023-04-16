55df55a12061881147431a6e47d199879876d4ae
Anzhelika Alpeeva <angel_and_ka@mail.ru>
2023-04-17 00:35:36 +0700

    задача 1

---
+++ OneDrive/Рабочий стол/GB Разработчик/С#/HomeWork2 C#/Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход 
+// трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+if (number < 100 || number >= 1000)
+{
+    Console.WriteLine("Вы ввели не трехзначное число, повторите ввод");
+    return;
+}
+Console.WriteLine($"Введеное число {number}");
+int secondNumber = number / 10 % 10;
+Console.WriteLine($"Вторая цифра {secondNumber}");
