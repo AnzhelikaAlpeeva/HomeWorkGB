1a2c79a2bf43cc2d4b2f4579aa20dfb953bfecfa
Anzhelika Alpeeva <angel_and_ka@mail.ru>
2023-04-19 01:52:26 +0700

    семинар4 - задачи

---
+++ OneDrive/Рабочий стол/GB Разработчик/С#/Sem4 C#/Program.cs
@@ -0,0 +1,73 @@
+﻿// палиндром для строк
+// string s = "12a21";
+
+// bool b = true;
+
+
+// for (int i = 0; i < s.Length / 2; i++)
+// {
+// 	if (s[i] != s[s.Length - 1 - i])
+// 	{
+// 		b = false; break;
+// 	}
+// }
+
+// Console.WriteLine(b);
+
+
+// Задача Напишите программу, которая принимает на вход 
+// число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+// System.Console.WriteLine("Введите число А: ");
+//   int A = int.Parse(Console.ReadLine());
+
+//   int getsum(int number)
+//   {
+//    int sum = 0;
+
+//   for (int i = 1; i <= A; i++)
+
+//   sum += i;
+//   return sum;
+// }
+
+// System.Console.WriteLine($"Сумма чисел от 1 до {A}^ {getsum (A)}");
+
+
+
+// Задача 26: Напишите программу, которая принимает 
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+
+// Console.WriteLine(byte.MaxValue);
+// Console.WriteLine(byte.MinValue); // 8
+// Console.WriteLine();
+// Console.WriteLine(short.MaxValue);
+// Console.WriteLine(short.MinValue); // 16
+// Console.WriteLine();
+// Console.WriteLine(int.MaxValue);
+// Console.WriteLine(int.MinValue); // 32
+// Console.WriteLine();
+// Console.WriteLine(long.MaxValue);
+// Console.WriteLine(long.MinValue); // 2^64
+
+
+
+// Задача 28: Напишите программу, которая 
+// принимает на вход число N и выдаёт 
+// произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+
+
+
+// Задача 30: Напишите программу, которая 
+// выводит массив из 8 элементов, заполненный 
+// нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
