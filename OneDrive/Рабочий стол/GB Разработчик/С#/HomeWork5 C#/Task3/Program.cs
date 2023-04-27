23e725a038f5b689bf817b2b6fa963522c71c578
Anzhelika Alpeeva <angel_and_ka@mail.ru>
2023-04-28 02:04:33 +0700

    task3 sem5

---
+++ OneDrive/Рабочий стол/GB Разработчик/С#/HomeWork5 C#/Task3/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементами массива.
+// [3,21 7,04 22,93 -2,71 78,24] -> 80,95
+
+double[] Array = {3.21, 7.04, 22.93, -2.71, 78.24};
+
+double diff(double[] Array)
+{
+    double minArr = Array[0];
+    double maxArr = Array[0];
+    for(int i = 1; i < Array.Length; i++)
+    {
+            if (Array[i] < minArr)
+        {
+            minArr = Array[i];
+        }
+        if (Array[i] > maxArr)
+        {
+            maxArr = Array[i];
+        }
+    }    
+    return maxArr-minArr; 
+}
+
+Console.WriteLine($"Разница между минимальным и максимальным значением массива: {diff(Array)}");
+
