83169afb47942c58ef9f5cd688d0e48e5556a0b5
Anzhelika Alpeeva <angel_and_ka@mail.ru>
2023-05-01 22:07:10 +0700

    task1

---
+++ OneDrive/Рабочий стол/GB Разработчик/С#/HomeWork6 C#/Task1/Program.cs
@@ -0,0 +1,33 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите количество элементов массива: ");
+int M = int.Parse(Console.ReadLine());
+int[] Array = new int[M];
+
+void EachElement(int M)
+{
+    for(int i = 0; i < M; i++)
+    {
+        Console.WriteLine($"Введите {i+1} элемент массива: ");
+        Array[i] = int.Parse(Console.ReadLine());
+    }
+}
+
+int MoreZero(int[] Array)
+{
+    int i = 0;
+    int sum = 0;
+    while(i < Array.Length)
+    {
+        if(Array[i] > 0)
+        sum = sum + 1;
+        i++;
+    }
+    return sum;
+}
+
+EachElement(M);
+Console.WriteLine($"Чисел больше нуля: {MoreZero(Array)}");
