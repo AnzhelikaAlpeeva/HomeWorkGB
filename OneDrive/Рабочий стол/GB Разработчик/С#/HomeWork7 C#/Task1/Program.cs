1a263fda97d1001e2399c2cde31644453421eaa8
Anzhelika Alpeeva <angel_and_ka@mail.ru>
2023-05-05 00:19:29 +0700

    task1

---
+++ OneDrive/Рабочий стол/GB Разработчик/С#/HomeWork7 C#/Task1/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задайте двумерный массив размером m×n, 
+// заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+System.Console.Write("Введите количество строк M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите количество столбцов N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+void CreateAndPrintMatrix(int a, int b)
+{
+    Random rnd = new Random();
+    double[,] arr = new double[a,b];
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            double add = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+            arr[i, j] = add;
+        }
+    }
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+CreateAndPrintMatrix(M, N);
