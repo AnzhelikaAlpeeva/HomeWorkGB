642b2dea2be5d2b437302ff42268dd70e634df77
Anzhelika Alpeeva <angel_and_ka@mail.ru>
2023-06-30 00:00:48 +0700

    task1

--- OneDrive/Рабочий стол/GB Разработчик/Python HW/HW2 Python/Task3.py
+++ OneDrive/Рабочий стол/GB Разработчик/Python HW/HW2 Python/Task3.py
@@ -2,16 +2,22 @@
 # вида 2k), не превосходящие числа N.
 # 10 -> 1 2 4 8
 
-N = int(input('Введите число N: '))
-flag = True
-number = 2
-for i in range(N):
-    if flag:
-        number = number ** i
-        if number <= N:
-            print(number)
-            number = 2
-        else:
-            flag = False
-    else:
-        i = N
+# N = int(input('Введите число N: '))
+# flag = True
+# number = 2
+# for i in range(N):
+#     if flag:
+#         number = number ** i
+#         if number <= N:
+#             print(number)
+#             number = 2
+#         else:
+#             flag = False
+#     else:
+#         i = N
+
+n = int(input())
+i = 0
+while 2 ** i <= n:
+    print(2 ** i)
+    i += 1
