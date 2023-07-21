32f1a06f49e864c4f9f09c0b571301a7455bf9f9
Anzhelika Alpeeva <angel_and_ka@mail.ru>
2023-07-21 16:34:26 +0700

    user

---
+++ OneDrive/Рабочий стол/GB Разработчик/Python HW/HW8 Python/user.py
@@ -0,0 +1,29 @@
+def choose_mode():
+    mode = input('Выберите режим работы со справочником: запись или поиск - ')
+    if mode == 'запись':
+        person = fill_person()
+    elif mode == 'поиск':
+        person = show_person()
+    else:
+        print('Такого режима не существует!')
+        choose_mode()
+    return person, mode
+
+
+def fill_person():
+    surname = input('Введите фамилию: ')
+    name = input('Введите имя: ')
+    patronym = input('Введите отчество: ')
+    phone = input('Введите телефон: ')
+    return surname, name, patronym, phone
+
+def show_person():
+    char = input('Введите фамилию/имя/отчество/телефон человека, которого ищите: ')
+    data = open('file_catalog/catalog.txt', 'r', encoding='utf-8')
+    text = data.read()
+    if char in text:
+        print('Такой человек есть в справочнике')
+    else:
+        print('Человек не найден')
+    print('\n')
+
